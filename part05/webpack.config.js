94306a1c6527e655eea5f9376a0a2da7a5d03b4d
wanghao <281463547@qq.com>
2022-06-29 22:26:35 +0800

    贪吃蛇环境搭建及主界面初始化

---
+++ part05/webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+//引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    //指定入口文件
+    entry: "./src/index.ts",
+    output: {
+        //打包文件所在目录
+        path: path.resolve(__dirname, 'dist'),
+        //打包后的文件名
+        filename: "bundle.js",
+        environment: {
+            //不使用箭头函数
+            arrowFunction: false
+        }
+    },
+    mode: 'development',
+    //指定打包时所使用的模块
+    module: {
+        rules: [
+            {
+                //规则生效的文件
+                test: /\.ts$/,
+                //加载器（执行顺序为倒序，ts->js->转译）
+                use: [
+                    //配置babel
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            //环境预设置
+                            presets: [
+                                [
+                                    //指定环境的插件
+                                    "@babel/preset-env",
+                                    //配置信息
+                                    {
+                                        //要兼容的目标浏览器
+                                        targets: {
+                                            "chrome": "90"
+                                        },
+                                        "corejs": "3",
+                                        //使用corejs的方式，usage 按需加载
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'],
+                //要排除的文件
+                exclude: /node_modules/
+            },
+
+            //设置less文件的处理
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    //引入postcss
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: 'last 2 version'
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+    //配置webpack插件
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            // title: 'webpack打包插件'
+            template: "./src/index.html"
+        }),
+    ],
+
+    //设置需要引用的模块
+    resolve: {
+        extensions: ['.ts', ".js"]
+    }
+}
